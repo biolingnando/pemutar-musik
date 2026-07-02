7fd4306007f0808492f1f815c5e4696535120f0a
RWIN R NANDO <53708431+biolingnando@users.noreply.github.com>
2026-07-02 16:35:47 +0700

    Add service worker with caching functionality
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'winoro-musick-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const cp = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp)).catch(()=>{});
+      return res;
+    }).catch(()=>{}))
+  );
+});
